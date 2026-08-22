ef45be97714c3b2775769724c7877beef3916ce9
deejaykey32-star <deejaykey32@gmail.com>
2026-08-22 03:14:02 +0200

    Fix React import and App path in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import { Component, ErrorInfo, ReactNode, StrictMode } from 'react';
+import React, { Component, ErrorInfo, ReactNode, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './index.css';
 
 interface Props {
